94fecd6744747a5f4bdc091d2d3f8398ac1d2c8d
Jack17kdb <jack@kali.Johnson.com>
2025-09-16 14:39:00 +0300

    update fetch users

--- fetch users.jsx
+++ fetch users.jsx
@@ -4,23 +4,38 @@ import "./style.css";
 const App = () => {
   const [users, setUsers] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [filteredUsers, setFilteredUsers] = useState([]);
+  const [term, setTerm] = useState("");
+
+  const filterInput = (event) => {
+    const term = event.target.value;
+    setTerm(term);
+    const filtered = users.filter((user) => {
+      return user.toLowerCase().includes(term);
+    });
+    setFilteredUsers(filtered);
+  };
 
   const fetchUsers = async () => {
     try {
-      setIsLoading(true);
       const url = "https://jsonplaceholder.typicode.com/users";
+      setIsLoading(true);
       const response = await fetch(url);
 
       if (!response.ok) {
-        throw new Error("Error fetching users");
+        throw new Error("Error fetching data");
       }
 
       const data = await response.json();
       const names = data.map((user) => user.name);
       setUsers(names);
+      setFilteredUsers(names)
     } catch (error) {
-      console.log("error fetching users ", error);
+      setError("Error fetching data");
+      console.log("Error fetching data ", error);
       setUsers([]);
+      setFilteredUsers([])
     } finally {
       setIsLoading(false);
     }
@@ -34,16 +49,26 @@ const App = () => {
     return <p>Fetching users....</p>;
   }
 
+  if (error) {
+    return <p>{error}</p>;
+  }
+
   return (
     <>
-      {users.length > 0 ? (
+      <input
+        type="text"
+        placeholder="Search users..."
+        value={term}
+        onChange={filterInput}
+      ></input>
+      {filteredUsers.length > 0 ? (
         <ol>
-          {users.map((user, index) => (
+          {filteredUsers.map((user, index) => (
             <li key={index}>{user}</li>
           ))}
         </ol>
       ) : (
-        "No users Found"
+        <p>No matching users</p>
       )}
     </>
   );
